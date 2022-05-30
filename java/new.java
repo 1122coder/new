79b03d3090b9d8e55f6600c1ae4594c0102323b2
1122coder <malikmuhammadkashif414@gmail.com>
2022-05-30 22:24:39 +0500

    blah

--- java/new.java
+++ java/new.java
@@ -1,7 +1,15 @@
-public abc {
-		public static void main(String[] args){
-			System.out.println("Hello from the java side");
-			int a,b,sum;
-			a=1;b=4;sum=a+b;
-		}
+public FinancialCalc {
+		 double computeFinalValue(double principle, double interestRate, int numOfYears)
+{
+    double finalValue = 0.0;
+    finalValue = principle * Math.pow((1 + interestRate), numOfYears);
+    return finalValue;
+}
+
+    public static void main(String[] args){
+
+        FinancialCalc obj = new FinancialCalc();
+        System.out.println("Hence the finalValue is" + obj.computeFinalValue(50.0, 0.3, 4));
+
+    }
 }
